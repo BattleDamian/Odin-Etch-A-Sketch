a3d1a5deae32d7be07155e5e4a700a21cf15795c
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2024-04-22 15:35:14 -0400

    Add grid using js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const container = document.getElementById('container');
 
 document.appendChild(container);
+
+let gridSize = 4;
+
+function addDiv() {
+    document.createElement('div');
+}
+
+function createGrid(gridSize) {
+    for (let index = 0; index < gridSize*gridSize; index++) {
+        container.appendChild(addDiv);
+    }
+}
+
+createGrid()
