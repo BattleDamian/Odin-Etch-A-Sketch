c7e8cb6fafe2feab4d148d27c5d2baeb837ecb0c
Minami Hasaki <20251864+MinamiHasaki@users.noreply.github.com>
2024-04-22 23:14:36 -0400

    Add grid logic

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const container = document.createElement('div');
 container.setAttribute('id', 'container');
+var containerSize = 400;
+container.style.height = `${containerSize}px`;
+container.style.width = `${containerSize}px`;
 document.body.appendChild(container);
 
 var gridSize = 4;
@@ -9,7 +12,14 @@ function createGrid(gridSize) {
     for (let i = 0; i < gridSize**2; i++) {
         let newDiv = document.createElement('div');
         newDiv.setAttribute('class', 'grid');
+        newDiv.style.height = `${containerSize / gridSize}px`;
+        newDiv.style.width = `${containerSize / gridSize}px`;
         container.appendChild(newDiv);
+    }
+    for (let i = 0; i < document.getElementsByClassName("grid").length; i++) {
+        document.getElementsByClassName("grid")[i].addEventListener("mouseover", () => {
+            document.getElementsByClassName("grid")[i].style.backgroundColor = "black";
+        });
     }
 }
 
